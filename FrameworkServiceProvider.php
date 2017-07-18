d8b06f537c33ad147d96f96521d4f49ad74f4fc7
Josh Benham <joshbenham@gmail.com>
2017-07-18 22:16:24 +1000

    Basic Service Provider

---
+++ FrameworkServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Culture\Framework;
+
+use Illuminate\Support\ServiceProvider;
+
+class FrameworkServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap the application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+
+    /**
+     * Register the application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
